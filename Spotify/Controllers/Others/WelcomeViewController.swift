b79ce3bb3d330d03e868087484263f3eadd4d3ec
dhanajitkapali <dhanajitkapali30@gmail.com>
2022-05-14 16:48:25 +0530

    designed the welcome screen

--- Spotify/Controllers/Others/WelcomeViewController.swift
+++ Spotify/Controllers/Others/WelcomeViewController.swift
@@ -9,28 +9,97 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
     
+    // MARK: - Private Data Members
     private let signInButton : UIButton = {
         let button = UIButton()
         button.backgroundColor = .white
+        button.layer.cornerRadius = 10
         button.setTitle("SignIn with Spotify", for: .normal)
-        button.setTitleColor(.blue, for: .normal)
+        button.setTitleColor(.black, for: .normal)
         return button
     }()
     
+    private let backGroundImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.image = UIImage(named: "albums_background")
+         return imageView
+    }()
+    
+    
+    private let overLayView : UIView = {
+       let view = UIView()
+        view.backgroundColor = .black
+        view.alpha = 0.7
+        return view
+    }()
+    
+    private let logoImageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.image = UIImage(named: "icon")
+         return imageView
+    }()
+    
+    
+    private let label : UILabel = {
+       let label = UILabel()
+        label.numberOfLines = 0
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize : 32, weight : .semibold)
+        label.text = "Listen to Millions\nof Songs on\nthe go."
+        return label
+    }()
+   
+}
+
+
+// MARK: - LifeCycle Methods
+extension WelcomeViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Spotify"
-        view.backgroundColor = .systemGreen
-        
+        view.backgroundColor = .black
+        view.addSubview(backGroundImageView)
+        view.addSubview(overLayView)
+         
         //add the signIn button
         view.addSubview(signInButton)
         signInButton.addTarget(self, action: #selector(didTapSignIn), for: .touchUpInside)
+        
+        view.addSubview(label)
+        view.addSubview(logoImageView)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        signInButton.frame = CGRect(x: 20, y: view.bottom - 50 - view.safeAreaInsets.bottom , width: view.width - 50, height: 50)
+        backGroundImageView.frame = view.bounds
+        overLayView.frame = view.bounds
+        signInButton.frame = CGRect(
+            x: 20,
+            y: view.bottom - 80 - view.safeAreaInsets.bottom ,
+            width: view.width - 50,
+            height: 50
+        )
+        logoImageView.frame = CGRect(
+            x: (view.width-120)/2,
+            y: (view.height-120)/2,
+            width: 120,
+            height: 120
+        )
+        label.frame = CGRect(
+            x: 30,
+            y: (view.height+150)/2,
+            width: view.width-60,
+            height: 150
+        )
     }
+}
+
+
+// MARK: - Private Methods
+private extension WelcomeViewController{
     
     @objc func didTapSignIn(){
         let vc = AuthViewController()
@@ -43,10 +112,7 @@ class WelcomeViewController: UIViewController {
         navigationController?.pushViewController(vc, animated: true)
         
     }
-
-}
-
-private extension WelcomeViewController{
+    
     func handleSignIn(success : Bool){
         //Login user or tell them for error
         guard success else {
